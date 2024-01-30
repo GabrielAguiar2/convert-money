a3b21410794f7f2cc3e755ca9469e06f1f955cee
GabrielAguiar2 <gabriel.maguiar2@gmail.com>
2024-01-30 07:27:43 -0400

    Iniciando JS

---
+++ scripts.js
@@ -0,0 +1,9 @@
+const convertButton = document.querySelector(".convert-button")
+console.log(convertButton)
+
+
+function convertValues(){
+    console.log("Funcionou")
+}
+
+//convertButton.addEventListener("click", convertValues)
